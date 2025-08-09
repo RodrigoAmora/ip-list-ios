203693c411011a2cd7a8def7a10363fc6e6dcc3b
RodrigoAmora <rodrigo.amora.freitas@gmail.com>
2025-08-09 18:55:47 -0300

    Project commit

---
+++ ip-list/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ip-list
+//
+//  Created by Rodrigo Amora on 09/08/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ ip-listTests/ip_listTests.swift
@@ -0,0 +1,17 @@
+//
+//  ip_listTests.swift
+//  ip-listTests
+//
+//  Created by Rodrigo Amora on 09/08/25.
+//
+
+import Testing
+@testable import ip_list
+
+struct ip_listTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
