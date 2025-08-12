dee0386fe7632d55a3070924956aff59b0fabec5
RodrigoAmora <rodrigo.amora.freitas@gmail.com>
2025-08-12 03:22:59 -0300

    Screens of devices and wifi info

---
+++ ip-list/Controllers/WifiInfoViewController.swift
@@ -0,0 +1,29 @@
+//
+//  WifiInfoViewController.swift
+//  ip-list
+//
+//  Created by Rodrigo Amora on 12/08/25.
+//
+
+import UIKit
+
+class WifiInfoViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
